2509ac1138864d8917931e2b5cc073aa9e78f6a6
techJayhawk <ljheidrick@gmail.com>
2015-07-29 11:14:09 -0500

    loaded gridview with dummy data

---
+++ app/src/main/java/com/heidritech/popularmovies/ImageAdapter.java
@@ -0,0 +1,78 @@
+package com.heidritech.popularmovies;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+/**
+ * Created by Levi on 7/28/2015.
+ */
+public class ImageAdapter extends BaseAdapter {
+    Context mContext;
+
+    public ImageAdapter(Context context)
+    {
+        mContext = context;
+    }
+
+    @Override
+    public int getCount() {
+        return imgId.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return position;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+
+        ImageView imageView;
+        View rowView;
+        LayoutInflater inflater = ( LayoutInflater )mContext.
+                getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+
+        if (convertView == null) {
+            // if it's not recycled, initialize some attributes
+            /*imageView = new ImageView(mContext);
+            imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setPadding(8, 8, 8, 8);*/
+            rowView = inflater.inflate(R.layout.image_cell, null);
+            imageView = (ImageView) rowView.findViewById(R.id.imageView1);
+
+
+        } else {
+            imageView = (ImageView) convertView;
+        }
+
+        imageView.setImageResource(imgId[position]);
+        return imageView;
+    }
+
+    private int [] imgId =
+            {
+                R.drawable.no_poseter_found,R.drawable.sample_2, R.drawable.sample_3,
+                    R.drawable.sample_4, R.drawable.sample_5,
+                    R.drawable.sample_6, R.drawable.sample_7,
+                    R.drawable.sample_0, R.drawable.sample_1,
+                    R.drawable.sample_2, R.drawable.sample_3,
+                    R.drawable.sample_4, R.drawable.sample_5,
+                    R.drawable.sample_6, R.drawable.sample_7,
+                    R.drawable.sample_0, R.drawable.sample_1,
+                    R.drawable.sample_2, R.drawable.sample_3,
+                    R.drawable.sample_4, R.drawable.sample_5,
+                    R.drawable.sample_6, R.drawable.sample_7
+            };
+}
--- app/src/main/java/com/heidritech/popularmovies/MainActivity.java
+++ app/src/main/java/com/heidritech/popularmovies/MainActivity.java
@@ -4,16 +4,33 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
 
 
 public class MainActivity extends ActionBarActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Picasso.with(context).load("http://i.imgur.com/DvpvklR.png").into(imageView);
+        GridView gridView;
+        gridView = (GridView) findViewById(R.id.gridview );
+        gridView.setAdapter(new ImageAdapter(this));
 
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(MainActivity.this, "" + position,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
