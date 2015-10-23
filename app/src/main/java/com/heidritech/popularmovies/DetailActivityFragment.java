7c4d5407732323a0e2ba751a4b7fe02aadd945c7
Levi <ljheidrick@gmail.com>
2015-10-23 06:25:41 -0500

    working on ui for playing the trailers

--- app/src/main/java/com/heidritech/popularmovies/DetailActivityFragment.java
+++ app/src/main/java/com/heidritech/popularmovies/DetailActivityFragment.java
@@ -1,16 +1,15 @@
 package com.heidritech.popularmovies;
 
 import android.annotation.TargetApi;
-import android.app.PendingIntent;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -18,7 +17,6 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.loopj.android.http.AsyncHttpClient;
-import com.loopj.android.http.AsyncHttpRequest;
 import com.loopj.android.http.JsonHttpResponseHandler;
 import com.squareup.picasso.Picasso;
 
@@ -96,7 +94,7 @@ public class DetailActivityFragment extends Fragment {
         AsyncHttpClient client = new AsyncHttpClient();
         client.get(url, new JsonHttpResponseHandler()
         {
-            @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+            @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
                 super.onSuccess(statusCode, headers, response);
@@ -104,12 +102,6 @@ public class DetailActivityFragment extends Fragment {
 
                 JSONArray jsonArray = null;
                 String youtubeURL = "https://www.youtube.com/watch?v=";
-
-                RelativeLayout relativeLayout = (RelativeLayout)getActivity().findViewById(R.id.relative_layout);
-                ImageButton imageButton = (ImageButton) getActivity().findViewById(R.id.play_trailer);
-
-
-
                 try {
                     jsonArray = response.getJSONArray("results");
                     System.out.println(jsonArray);
@@ -127,34 +119,54 @@ public class DetailActivityFragment extends Fragment {
                             arrayList.add(videoName);
                             videoKeyArrayList.add(videoKey);
                             youtubeURL += videoKey;
-                            name = (TextView)getActivity().findViewById(R.id.trailer_name);
-                            name.setText(videoName);
+                            ImageButton imageButton; //= (ImageButton) getActivity().findViewById(R.id.relative_play);
 
+                            LinearLayout layout = (LinearLayout)getActivity().findViewById(R.id.trailer_layout);
+                            LinearLayout horizontal = new LinearLayout(getActivity());
+                            horizontal.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+                            horizontal.setOrientation(LinearLayout.HORIZONTAL);
+                            layout.addView(horizontal);
+                            RelativeLayout relativeLayout = new RelativeLayout(getActivity());
+                            relativeLayout.setLayoutParams(new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+                            horizontal.addView(relativeLayout);
+                            //imageButton = new ImageButton(getActivity());
+                            imageButton = new ImageButton(getActivity());
+                            name = new TextView(getActivity());
+                            imageButton.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+                            imageButton.setImageResource(R.drawable.play1);
+                            imageButton.setBackgroundResource(0);
+                            imageButton.setId(i);
+                            /*RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);*/
+                            horizontal.addView(imageButton);
+                            name.setText(videoName);
+                            name.setGravity(Gravity.BOTTOM);/*
+                            params.addRule(RelativeLayout.ALIGN_RIGHT, imageButton.getId());*/
+                            /*name.setLayoutParams(params);*/
+
+                            horizontal.addView(name);
 
                             final String finalYoutubeURL = youtubeURL;
                             imageButton.setOnClickListener(new View.OnClickListener() {
                                 @Override
                                 public void onClick(View v) {
                                     startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(finalYoutubeURL)));
+
                                 }
                             });
 
-                            imageButton.setImageResource(R.drawable.play);
-                            imageButton.setBackground(null);
+
+
+
+
+
+
 
 
                         }
-                        int j = 1;
 
-                        RelativeLayout.LayoutParams params1 = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-                        imageButton = new ImageButton(getActivity());
-                        params1.addRule(RelativeLayout.BELOW, imageButton.getId() - 1);
 
-                        imageButton.setId(j);
 
-                        relativeLayout.addView(imageButton, params1);
 
-                        j++;
 
                     }
                 } catch (JSONException e) {
@@ -163,5 +175,7 @@ public class DetailActivityFragment extends Fragment {
             }
         });
     }
+
+
 
 }
